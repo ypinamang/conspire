95fb921cb8ba2199b104fd4d3d7798f538b91f52
Yaw <ypinamangyapong@gmail.com>
2025-07-16 21:38:04 +0900

    Make profile image clickable

---
+++ js/pageControl.js
@@ -0,0 +1,7 @@
+const profileImage = document.querySelector(".profile-pic");
+
+profileImage.addEventListener("click", updateAvatar);
+
+function updateAvatar() {
+    alert("Hold on. You will soon be able to change your profile picture.")
+}
